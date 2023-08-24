dc628d84d1660f005a373e27edb8936b979b3a0f
casagrande7 <joaofootz10@gmail.com>
2023-08-24 15:26:33 -0300

    Atualizado

--- estudante.js
+++ estudante.js
@@ -5,5 +5,8 @@ class Estudante {
         this.idade = idade;
         this.notas = notas;
     }
+    notasDosAlunos() {
+        let media = [8, 7.5, 9.2, 6.8, 9.5];
+    }
 }
-let maria = new Estudante("Maria", 20, [8, 7.5, 9.2, 6.8, 9.5]);
+let maria = new Estudante("Maria", 20, [8 + 7.5 + 9.2 + 6.8 + 9.5]);
