d9e6ea144f9217eae90b6bd75a9ed010b401ac04
casagrande7 <joaofootz10@gmail.com>
2023-08-24 15:26:25 -0300

    Atualizado

---
+++ estudante.js
@@ -0,0 +1,9 @@
+"use strict";
+class Estudante {
+    constructor(nome, idade, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.notas = notas;
+    }
+}
+let maria = new Estudante("Maria", 20, [8, 7.5, 9.2, 6.8, 9.5]);
---
+++ estudante.ts
@@ -0,0 +1,25 @@
+class Estudante{
+    nome: string;
+    idade: number;
+    notas: number[];
+
+    constructor(nome: string, idade: number, notas: number[]){
+        this.nome = nome;
+        this.idade = idade;
+        this.notas = notas;
+}
+
+notasDosAlunos(){
+  let media: number[] = [8 ,  7.5 ,  9.2 , 6.8 , 9.5]
+  
+  
+}
+
+
+
+
+
+    
+}
+
+let maria = new Estudante("Maria", 20, [8+ 7.5+ 9.2+ 6.8+ 9.5]);
