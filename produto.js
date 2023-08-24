f5d9730b2bfc605e27b03d65ac8795c3f5292297
casagrande7 <joaofootz10@gmail.com>
2023-08-24 10:23:06 -0300

    Atualizado

--- produto.js
+++ produto.js
@@ -5,8 +5,18 @@ class Produto {
         this.valor = valor;
     }
     marketing() {
-        return "O produto vendido é um " + this.nome + " que custa R$" + this.valor;
+        return "O produto vendido é um " + this.nome + " que custa R$ " + this.valor;
+    }
+    desconto(a) {
+        return "Desconto de 20% aplicado, Novo valor: R$ " + (this.valor - (a * this.valor) / 100);
     }
 }
 let tenis = new Produto("Tênis", 350.00);
+let iphone = new Produto("Iphone Pro Max 11", 5000.00);
+let notebook = new Produto("Notebook Samsung Book i5", 3000.00);
 console.log(tenis.marketing());
+console.log(tenis.desconto(20));
+console.log(iphone.marketing());
+console.log(iphone.desconto(20));
+console.log(notebook.marketing());
+console.log(notebook.desconto(20));
