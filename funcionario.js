0805f72f5e2c61fede966f887235285e2e1dd7ac
casagrande7 <joaofootz10@gmail.com>
2023-08-29 12:27:06 -0300

    Atualizado

--- funcionario.js
+++ funcionario.js
@@ -7,23 +7,33 @@ class Funcionario {
         this.inss = inss;
         this.ir = ir;
     }
-    aumento(aumento) {
-        this.salario_bruto = this.salario_bruto + (this.salario_bruto * aumento) / 100;
-        return "Funcionária: " + this.nome + " exerço o cargo: " + this.cargo + " ,aumento de 10% aplicado. Novo salário: " + this.salario_bruto + " reais";
-    }
-    taxas() {
-        this.salario_bruto = (this.salario_bruto * 100) / 100;
-        this.inss = (this.salario_bruto * this.inss) / 100;
-        this.ir = (this.salario_bruto * this.ir) / 100;
+    impostos(inss, ir) {
+        this.inss = (this.salario_bruto * inss) / 100;
+        let salariobruto = this.salario_bruto - this.inss;
+        this.ir = (salariobruto * ir) / 100;
         let impostos = this.inss + this.ir;
-        let salarioliquido = this.salario_bruto - this.inss;
-        let salarioliquido2 = salarioliquido - this.ir;
-        return "Funcionária: " + this.nome + " Salário Bruto: " + this.salario_bruto + " Impostos a serem pagos: " + impostos + " Salário líquido: " + salarioliquido2;
+        let salarioliquido = this.salario_bruto - impostos;
+        return "Funcionária: " + this.nome + " Salário Bruto: " + this.salario_bruto.toFixed(2) + " Impostos a serem pagos: " + impostos.toFixed(2) + " Salário líquido: " + salarioliquido.toFixed(2);
+    }
+    aumento(aumento) {
+        let novosalario = (this.salario_bruto * aumento) / 100;
+        this.salario_bruto = this.salario_bruto + novosalario;
+        return "Novo salário com 10% de aumento: " + this.salario_bruto;
+    }
+    taxas(inss, ir) {
+        this.inss = (this.salario_bruto * inss) / 100;
+        let salarionovo = this.salario_bruto - this.inss;
+        this.ir = (salarionovo * ir) / 100;
+        let taxas = this.inss + this.ir;
+        let salarioliquido = this.salario_bruto - taxas;
+        return "Funcionária: " + this.nome + " Salário Bruto: " + this.salario_bruto + " Impostos a serem pagos: " + taxas.toFixed(2) + " Salário líquido: " + salarioliquido.toFixed(2);
     }
 }
 let funcionario = new Funcionario("Ana Silva", "Analista de Vendas", 3000.00, 11, 7.5);
 let operario = new Funcionario("João Pedro", "Analista de Vendas", 5300.00, 11, 7.5);
+console.log(funcionario.impostos(11, 7.5));
 console.log(funcionario.aumento(10));
-console.log(funcionario.taxas());
+console.log(funcionario.taxas(11, 7.5));
+console.log(operario.impostos(11, 7.5));
 console.log(operario.aumento(10));
-console.log(operario.taxas());
+console.log(operario.taxas(11, 7.5));
--- funcionario.ts
+++ funcionario.ts
@@ -13,21 +13,28 @@ class Funcionario{
         this.ir = ir;
         
     }
-    aumento(aumento: number){
-        this.salario_bruto = this.salario_bruto + (this.salario_bruto*aumento)/100;
-        return "Funcionária: " + this.nome + " exerço o cargo: " + this.cargo + " ,aumento de 10% aplicado. Novo salário: "+ this.salario_bruto + " reais";
-    }
-    taxas(){
-        this.salario_bruto = (this.salario_bruto*100)/100
-        this.inss = (this.salario_bruto*this.inss)/100 
-        this.ir = (this.salario_bruto*this.ir)/100;
+    impostos(inss: number, ir: number){
+        this.inss = (this.salario_bruto*inss)/100 
+        let salariobruto = this.salario_bruto - this.inss;
+        this.ir = (salariobruto*ir)/100
         let impostos = this.inss + this.ir
-        let salarioliquido = this.salario_bruto - this.inss
-        let salarioliquido2 = salarioliquido - this.ir
+        let salarioliquido = this.salario_bruto - impostos
+        return "Funcionária: " + this.nome + " Salário Bruto: "+ this.salario_bruto.toFixed(2) + " Impostos a serem pagos: " + impostos.toFixed(2)  + " Salário líquido: " + salarioliquido.toFixed(2)  ;
         
+    }
+    aumento(aumento: number){
+        let novosalario = (this.salario_bruto*aumento)/100 
+        this.salario_bruto = this.salario_bruto + novosalario
+        return "Novo salário com 10% de aumento: " + this.salario_bruto 
+    }
+    taxas(inss: number, ir: number){
+        this.inss = (this.salario_bruto*inss)/100
+        let salarionovo = this.salario_bruto - this.inss;
+        this.ir = (salarionovo*ir)/100;
+        let taxas = this.inss + this.ir
+        let salarioliquido = this.salario_bruto - taxas
+        return "Funcionária: " + this.nome + " Salário Bruto: "+ this.salario_bruto + " Impostos a serem pagos: " + taxas.toFixed(2) + " Salário líquido: " + salarioliquido.toFixed(2)  ;
         
-
-        return "Funcionária: " + this.nome + " Salário Bruto: "+ this.salario_bruto + " Impostos a serem pagos: " +  impostos + " Salário líquido: " + salarioliquido2 ;
     }
     
 }
@@ -36,8 +43,13 @@ class Funcionario{
 let funcionario = new Funcionario("Ana Silva", "Analista de Vendas", 3000.00, 11, 7.5 );
 let operario = new Funcionario("João Pedro", "Analista de Vendas", 5300.00, 11, 7.5 );
 
+console.log(funcionario.impostos(11, 7.5));
 console.log(funcionario.aumento(10));
-console.log(funcionario.taxas());
+console.log(funcionario.taxas(11, 7.5));
 
+console.log(operario.impostos(11, 7.5));
 console.log(operario.aumento(10));
-console.log(operario.taxas());
+console.log(operario.taxas(11, 7.5));
+
+
+
