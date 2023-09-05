c3c1e1912d7c48b61391602b1dc22ee9601afbf0
casagrande7 <joaofootz10@gmail.com>
2023-09-05 08:25:48 -0300

    Atualizado

--- playlist.ts
+++ playlist.ts
@@ -23,10 +23,32 @@ class Musica{
 
     reproduzir(){
         if(this.musicas.length > 0){
+            this.reproduzindo = true; 
+            return "Reproduzindo" + this.musicas[this.indiceAtual].titulo + ' - ' + this.musicas[this.indiceAtual].artista;
 
         }
         else{
             return "A playlist está vazia";
+        }
+    }
+
+    pausar(){
+        if(this.reproduzindo == true){
+            this.reproduzindo = false;
+            return "Música pausada";
+        }
+        else{
+            return "Nenhuma música está sendo reproduzida";
+        }
+    }
+
+    proxima(){
+        if(this.musicas.length > 0){
+            this.indiceAtual = (this.indiceAtual + 1) % this.musicas.length;
+            return this.reproduzir();
+        }
+        else{
+            return "A Playlist está vazia";
         }
     }
 
