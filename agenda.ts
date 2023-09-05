9af145c0f90b1ba350f31a78d5189758423cfb5c
casagrande7 <joaofootz10@gmail.com>
2023-09-05 12:18:26 -0300

    Atualizado

--- agenda.ts
+++ agenda.ts
@@ -23,8 +23,8 @@ class Agenda{
 
     visualizarAgenda(){
         console.log("Eventos disponíveis");
-        for(let i=0; i<= this.eventos.length; i++){
-            console.log(this.eventos[i].nome + " na data " + this.eventos[i].data + " ás " + this.eventos[i].horario );
+        for(let i=0; i< this.eventos.length; i++){
+            console.log(this.eventos[i].nome + " na data " + this.eventos[i].data + " ás " + this.eventos[i].horario);
         }
        
     }
@@ -37,7 +37,7 @@ class Agenda{
 }
 
 const evento1 = new Evento("Halloween", "17/10/2023", "22:30");
-const evento2 = new Evento("Halloween", "18/10/2023", "21:30");
+const evento2 = new Evento("Casamento", "18/10/2023", "21:30");
 const evento3 = new Evento("Carnaval", "19/10/2023", "20:30");
 
 
@@ -45,6 +45,9 @@ const minhaAgenda = new Agenda();
 minhaAgenda.adicionarEvento(evento1);
 minhaAgenda.adicionarEvento(evento2);
 minhaAgenda.adicionarEvento(evento3);
+minhaAgenda.removerEvento(evento1);
+console.log(minhaAgenda.visualizarAgenda());
 
-console.log(minhaAgenda.visualizarAgenda())
+
+
 
